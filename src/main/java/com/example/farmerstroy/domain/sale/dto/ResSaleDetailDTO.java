0c5a07121448038ba82e2219f557b81f3637cc66
devjyu <devjyu@gmail.com>
2023-08-17 10:52:31 +0900

    ResSaleDetailDTO

---
+++ src/main/java/com/example/farmerstroy/domain/sale/dto/ResSaleDetailDTO.java
@@ -0,0 +1,58 @@
+package com.example.farmerstroy.domain.sale.dto;
+
+import java.util.List;
+
+import com.example.farmerstroy.model.sale.entity.SaleEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@ToString
+public class ResSaleDetailDTO {
+    private List<Sale> saleDetailList;
+
+    public static ResSaleDetailDTO of(List<SaleEntity> saleEntityList) {
+        return ResSaleDetailDTO.builder()
+                .saleDetailList(Sale.fromEntityList(saleEntityList))
+                .build();
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    public static class Sale {
+        private Long idx;
+        private String title;
+        private String introduction;
+        private String price;
+        private String saleImg;
+        private Integer amount;
+        private Long categoryIdx;
+
+        public static Sale fromEntity(SaleEntity saleEntity) {
+            return Sale.builder()
+                    .idx(saleEntity.getIdx())
+                    .title(saleEntity.getTitle())
+                    .introduction(saleEntity.getIntroduction())
+                    .price(saleEntity.getPrice())
+                    .saleImg(saleEntity.getSaleImg())
+                    .categoryIdx(saleEntity.getCategoryEntity().getIdx())
+                    .build();
+        }
+
+        public static List<Sale> fromEntityList(List<SaleEntity> saleEntityList) {
+            return saleEntityList
+                    .stream()
+                    .map((saleEntity) -> Sale.fromEntity(saleEntity))
+                    .toList();
+        }
+    }    
+}
