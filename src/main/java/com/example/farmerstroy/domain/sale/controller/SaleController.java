8cbe2247bd3ef0eb072ca8d8df0e5adc0fff99c5
devjyu <devjyu@gmail.com>
2023-08-17 13:51:19 +0900

    불필요import삭제

--- src/main/java/com/example/farmerstroy/domain/sale/controller/SaleController.java
+++ src/main/java/com/example/farmerstroy/domain/sale/controller/SaleController.java
@@ -1,6 +1,5 @@
 package com.example.farmerstroy.domain.sale.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
