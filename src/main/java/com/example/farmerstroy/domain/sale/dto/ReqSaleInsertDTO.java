c93a1e19fa181bc9cd7dc82a17fbdb4704c0d094
devjyu <devjyu@gmail.com>
2023-08-18 21:10:03 +0900

    setter 추가

--- src/main/java/com/example/farmerstroy/domain/sale/dto/ReqSaleInsertDTO.java
+++ src/main/java/com/example/farmerstroy/domain/sale/dto/ReqSaleInsertDTO.java
@@ -9,12 +9,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
+@Setter
 @ToString
 public class ReqSaleInsertDTO {
 
@@ -40,5 +42,6 @@ public class ReqSaleInsertDTO {
 
     @NotNull
     private Long categoryIdx;
+
 }
 
