2f740a6561821560db596fba192ab6c0bdf270dc
[devjyu] <[devjyu@gmail.com]>
2023-08-29 14:58:22 +0900

    커뮤니티 전체 게시글 리스트 가져오기

--- src/main/java/com/example/farmerstroy/domain/community/service/CommunityService.java
+++ src/main/java/com/example/farmerstroy/domain/community/service/CommunityService.java
@@ -1,8 +1,12 @@
 package com.example.farmerstroy.domain.community.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.farmerstroy.domain.community.dto.ResCommunityDTO;
+import com.example.farmerstroy.model.community.entity.CommunityEntity;
 import com.example.farmerstroy.model.community.repository.CommunityRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +18,12 @@ public class CommunityService {
     
     private final CommunityRepository communityRepository;
 
+    // 커뮤니티 전체 게시글 가져오기
+    public ResCommunityDTO getCommunityData() {
+        List<CommunityEntity> communityEntityList = communityRepository.findAll();
+        ResCommunityDTO dto = ResCommunityDTO.builder()
+        .communityList(ResCommunityDTO.Community.fromEntityList(communityEntityList))
+        .build();
+        return dto;
+    }
 }
