dee010dbbdd4d894533e8c5c0355cee2122b6d63
[devjyu] <[devjyu@gmail.com]>
2023-08-29 13:40:55 +0900

    jpa쿼리 추가

--- src/main/java/com/example/farmerstroy/model/community/repository/CommunityRepository.java
+++ src/main/java/com/example/farmerstroy/model/community/repository/CommunityRepository.java
@@ -1,9 +1,14 @@
 package com.example.farmerstroy.model.community.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.farmerstroy.model.community.entity.CommunityEntity;
 
 public interface CommunityRepository extends JpaRepository<CommunityEntity, Long>{
+
+    Optional<CommunityEntity> findByIdx(Long idx);
+
     
 }
