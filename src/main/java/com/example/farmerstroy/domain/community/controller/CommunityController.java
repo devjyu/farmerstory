9da1e0bb00d6d9ceb6de4f0047021bf144c736f5
[devjyu] <[devjyu@gmail.com]>
2023-08-29 14:59:26 +0900

    화면 controller 완성

--- src/main/java/com/example/farmerstroy/domain/community/controller/CommunityController.java
+++ src/main/java/com/example/farmerstroy/domain/community/controller/CommunityController.java
@@ -1,13 +1,24 @@
 package com.example.farmerstroy.domain.community.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.farmerstroy.domain.community.dto.ResCommunityDTO;
+import com.example.farmerstroy.domain.community.service.CommunityService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class CommunityController {
+
+    private final CommunityService communityService;
     
     @GetMapping("/community")
-    public String communityPage(){
+    public String communityPage(Model model){
+        ResCommunityDTO resCommunityDTOList = communityService.getCommunityData();
+        model.addAttribute("resCommunityDTOList", resCommunityDTOList);
         return "community";
     }
 }
